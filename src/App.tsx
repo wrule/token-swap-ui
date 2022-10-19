ec3650696d76a68e8071cc3c26882664308f9cf4
jimao <jimao@jimaodeMacBook-Pro.local>
2022-10-19 18:44:42 +0800

    提交代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import { ConfigProvider, Button } from 'antd';
+import { ConfigProvider, Layout } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 import 'antd/dist/antd.min.css';
+import './App.scss';
+import style from './App.module.scss';
+
+const { Header, Content } = Layout;
 
 moment.locale('zh-cn');
 
 function App() {
   return (
     <ConfigProvider locale={zhCN}>
-      <Button>你好，世界</Button>
+      <Layout style={{ height: '100%' }}>
+        <Header>
+          <h1 className={style.title}>Token Swap</h1>
+        </Header>
+        <Content className={style.content}>Content</Content>
+      </Layout>
     </ConfigProvider>
   );
 }
